25e7cc5dbfa12337450edea77774ce21166af1b8
Krzysztof Dąbrowski <krzysdabro@live.com>
2020-07-19 18:41:21 +0200

    zsh: update git prompt

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -1,21 +1,22 @@
 __git_ps1() {
-  _branch=$(git symbolic-ref HEAD 2> /dev/null) || exit
+  _branch=$(git symbolic-ref HEAD 2> /dev/null) || \
+  _branch=$(git rev-parse --short HEAD 2> /dev/null) || exit
 
   # worktree status
   _color="green"
   $(git diff --no-ext-diff --quiet --cached) || _color="yellow"
   $(git diff --no-ext-diff --quiet) || _color="red"
 
-  # branch status
+  # remote status
   local=$(git rev-parse @ 2> /dev/null)
   remote=$(git rev-parse @{u} 2> /dev/null)
   base=$(git merge-base @ @{u} 2> /dev/null)
 
-  if [[ "$(git remote show)" == "" ]]; then _status="×"
-  elif [[ $local == $remote ]]; then _status="≡"
-  elif [[ $local == $base ]]; then _status="↓"
-  elif [[ $remote == $base ]]; then _status="↑"
-  else _status="↕" fi
+  if [[ $remote == "" ]]; then _status="×"                # Missing
+  elif [[ $local == $remote ]]; then _status="≡"          # Up to date
+  elif [[ $local == $base ]]; then _status="↓"            # Behind
+  elif [[ $remote == $base ]]; then _status="↑"           # Ahead
+  else _status="↕" fi                                     # Diverged
 
 
   echo " [%F{$_color}${_branch##refs/heads/}%f $_status]"
